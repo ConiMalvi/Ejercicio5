d34050e670572f77e506f714c51350a01182a3e3
ConiMalvi <constanza.dev@gmail.com>
2023-03-29 22:51:48 -0300

    AlgoritmosIvaHipoIMC

---
+++ Ejercicios.js
@@ -0,0 +1,21 @@
+
+//IMC
+var peso = 90;
+var altura = 1.78;
+var imc = calcularIMC(peso, altura);
+console.log("El IMC es: " + imc);
+
+//IVA
+const tasaIVA = 0.19;
+var cantidad = 100;
+var precioTotal = cantidad + (cantidad * tasaIVA);
+
+console.log("El precio total con IVA es: " + precioTotal);
+
+//HIPOTENUSA
+
+var cateto1 = 3;
+var cateto2 = 4;
+var hipotenusa = Math.sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+
+console.log("La hipotenusa del triángulo es: " + hipotenusa);
